f5a6ab46a7cb499c3b787ca7de0e11112dcc6023
rameshhb37 <rameshhb37@gmail.com>
2024-02-07 23:41:05 +0530

    feature: initial project setup commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+
+connectDB();
+
+app.get("/", (req, res) => res.send("API is running"));
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/posts", require("./routes/posts"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/profile", require("./routes/profile"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('servers is running!!'))
